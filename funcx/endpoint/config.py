a3553d329bae8a48c4ad37ac866b83b28ea29593
April <aprilyanli38@gmail.com>
2019-06-26 14:32:52 -0500

    changed config for wisc

--- funcx/endpoint/config.py
+++ funcx/endpoint/config.py
@@ -7,7 +7,7 @@ from parsl.app.app import python_app, bash_app
 from parsl.launchers import SingleNodeLauncher
 from parsl.channels import LocalChannel
 from parsl.channels import SSHInteractiveLoginChannel
-from parsl.providers import LocalProvider
+from parsl.providers import LocalProvider, CondorProvider
 from parsl.executors import HighThroughputExecutor
 
 # GlobusAuth-related secrets
@@ -42,22 +42,79 @@ def _get_parsl_config():
         (parsl.config.Config): Parsl config to execute tasks.
     """
 
+    # config = Config(
+    #     executors=[
+    #         HighThroughputExecutor(
+    #             label="htex_local",
+    #             worker_debug=False,
+    #             poll_period=1,
+    #             cores_per_worker=1,
+    #             max_workers=1,
+    #             provider=LocalProvider(
+    #                 channel=LocalChannel(),
+    #                 init_blocks=1,
+    #                 max_blocks=1,
+    #                 min_blocks=1,
+    #             ),
+    #         )
+    #     ],
+    #     strategy=None
+    # )
+
+    x509_proxy = 'x509up_u%s'%(os.getuid())
+
+    wrk_init = '''
+    export XRD_RUNFORKHANDLER=1
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh
+    export PATH=`pwd`/.local/bin:$PATH
+    export PYTHONPATH=`pwd`/.local/lib/python3.6/site-packages:$PYTHONPATH
+
+    export X509_USER_PROXY=`pwd`/%s
+    mkdir -p ./coffea_parsl_condor
+    '''%(x509_proxy)
+
+    twoGB = 2048
+    nproc = 4
+
+    condor_cfg = '''
+    transfer_output_files = coffea_parsl_condor
+    RequestMemory = %d
+    RequestCpus = %d
+    ''' % (twoGB*nproc, nproc)
+
+    xfer_files = ['%s/.local' % (os.environ['HOME'], ), '%s/%s' % (os.environ['HOME'], x509_proxy, )]
+
+    #envs={'PYTHONPATH':'/afs/hep.wisc.edu/home/lgray/.local/lib/python3.6/site-packages:%s'%os.environ['PYTHONPATH'],
+    #      'X509_USER_PROXY':'./%s'%x509_proxy,
+    #      'PATH':'/afs/hep.wisc.edu/home/lgray/.local/bin:%s'%os.environ['PATH']}
+
     config = Config(
         executors=[
             HighThroughputExecutor(
-                label="htex_local",
-                worker_debug=False,
-                poll_period=1,
+                label="coffea_parsl_condor",
+                address=address_by_hostname(),
+                worker_debug=True,
+                prefetch_capacity=0,
                 cores_per_worker=1,
-                max_workers=1,
-                provider=LocalProvider(
+                max_workers=nproc,
+                worker_logdir_root='./',
+                provider=CondorProvider(
                     channel=LocalChannel(),
-                    init_blocks=1,
-                    max_blocks=1,
-                    min_blocks=1,
+                    init_blocks=4,
+                    max_blocks=4,
+                    #init_blocks=32,
+                    #max_blocks=64,
+                    nodes_per_block=1,
+                    worker_init = wrk_init,                
+                    transfer_input_files=xfer_files,
+                    scheduler_options=condor_cfg,
+                    cmd_timeout=200
                 ),
             )
         ],
-        strategy=None
+        retries = 10,
+        app_cache = True,
+        strategy = 'simple'
     )
+
     return config
